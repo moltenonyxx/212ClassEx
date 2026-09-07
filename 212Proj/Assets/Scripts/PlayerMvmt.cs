79ad4b468a8cefc3b7b635aec19d1609a646a67c
moltenonyxx <everettrivenburg@gmail.com>
2026-09-07 12:54:31 -0400

    added coded rotation in class

--- 212Proj/Assets/Scripts/PlayerMvmt.cs
+++ 212Proj/Assets/Scripts/PlayerMvmt.cs
@@ -1,13 +1,20 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMvmt : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float rotateSpeed = 120f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
+
+
+
+
+
 
     // Update is called once per frame
     void Update()
@@ -15,6 +22,16 @@ public class PlayerMvmt : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         Vector3 Move = new Vector3(x, y, 0f);
+        //transform.Rotate(0.8f, 0f, 0f);
         transform.Translate(Move * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0f, 0f, rotateSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0f, 0f, -rotateSpeed * Time.deltaTime);
+        }
     }
 }
