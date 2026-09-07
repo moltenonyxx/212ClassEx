c7e63d78df2bfe0745c6da7129d7aec69894cf4f
moltenonyxx <everettrivenburg@gmail.com>
2026-09-07 13:36:15 -0400

    added collision and debug for it

--- 212Proj/Assets/Scripts/PlayerMvmt.cs
+++ 212Proj/Assets/Scripts/PlayerMvmt.cs
@@ -4,27 +4,23 @@ using UnityEngine;
 public class PlayerMvmt : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
-    [SerializeField] float rotateSpeed = 120f;
+    [SerializeField] float rotateSpeed = 175f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-
-
-
-
-
     // Update is called once per frame
     void Update()
     {
+        //movement
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         Vector3 Move = new Vector3(x, y, 0f);
-        //transform.Rotate(0.8f, 0f, 0f);
         transform.Translate(Move * moveSpeed * Time.deltaTime);
 
+        //rotate
         if (Input.GetKey(KeyCode.Q))
         {
             transform.Rotate(0f, 0f, rotateSpeed * Time.deltaTime);
@@ -32,6 +28,15 @@ public class PlayerMvmt : MonoBehaviour
         if (Input.GetKey(KeyCode.E))
         {
             transform.Rotate(0f, 0f, -rotateSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collision happened " + collision.gameObject.name);
+        if (collision.collider.CompareTag("Obstacle"))
+        {
+            Debug.Log("you crashed");
         }
     }
 }
